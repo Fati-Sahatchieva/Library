95bb715cfe01ccbfeea6c7632fb2a3546e087069
Fati Sahatchieva <133803429+Fati-Sahatchieva@users.noreply.github.com>
2024-09-25 17:16:15 +0300

    new

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -87,8 +87,7 @@ namespace Library.Controllers
         }
 
         // POST: Books/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+      
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("BookId,Title,Author,PublicationYear,BookCopies,CategoryId")] Book book)
